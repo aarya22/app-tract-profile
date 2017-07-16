9edb10c1090561c7145393b5a0cfeda027720344
Aman Arya <aman.arya524@gmail.com>
2017-07-16 15:32:43 -0400

    Update main.py
    remove unecessary code

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def main():
     else:
         dti_params = {'FA': './dti_FA.nii.gz',
                       'params': './dti_params.nii.gz'}
+       
     FA_img = nib.load(dti_params['FA'])
     FA_data = FA_img.get_data()
     print("Extracting tract profiles...")
@@ -28,17 +29,7 @@ def main():
     path = os.getcwd() + '/profile/'
     if not os.path.exists(path):
         os.makedirs(path)
-    """
-    for t in os.listdir(tracks):
-        if t.endswith('.tck'):
-            tg = nib.streamlines.load(tracks+'/'+t)
-            streamlines = list(tg.streamlines)
-            profile = seg.calculate_tract_profile(FA_data, streamlines)
-            profile = profile.tolist()
-            t = os.path.splitext(os.path.basename(t))[0] #remove the .tck from string
-            p = path+'/'+t+'.json'
-            json.dump(profile, codecs.open(p, 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
-    """
+
     tg = nib.streamlines.load(tracks)
     streamlines = tg.streamlines
     profile = seg.calculate_tract_profile(FA_data, streamlines)
@@ -46,4 +37,5 @@ def main():
     t = os.path.splitext(os.path.basename(tracks))[0] #remove the .tck from string
     p = path+'/'+t+'.json'
     json.dump(profile, codecs.open(p, 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
+    
 main()
